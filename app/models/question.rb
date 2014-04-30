807230b2967c7216bf51c05eafbd174db32a8888
Vasiliy Troyan <trovas@inbox.ru>
2014-04-30 15:04:17 +0700

    Correct validates

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,7 @@
 class Question < ActiveRecord::Base
-	validates_length_of :title, minimum: 15
-	validates_presence_of :title
-	validates_presence_of :body
+  validates :title, length: {minimum: 15}
+  validates :title, presence: true
+  validates :body, presence: true
 	has_many :answers
   has_many :comments, as: :commentable
   has_and_belongs_to_many :tags
