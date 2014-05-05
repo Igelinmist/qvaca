0569eaa7f941debf446d1a43001513dd53313384
Vasiliy Troyan <trovas@inbox.ru>
2014-05-05 20:47:29 +0700

    Anybody can view question list

--- spec/acceptance/questions_view_list_spec.rb
+++ spec/acceptance/questions_view_list_spec.rb
@@ -5,10 +5,18 @@ feature 'view question list', %q{
   As a non authenticate user
   I want to see the list of questions
   } do
-  before {create_list(:question, 2)}
+  # before {create_list(:question, 2)}
+  given!(:questions) { create_list(:question, 2) }
   scenario "Anyone is able to view the list of question" do
     visit questions_path
-    #save_and_open_page
-    expect(page).to have_content('MyStringFifteenMin')
+    # save_and_open_page
+    expect(page).to have_content(questions[0].title)
+    expect(page).to have_content(questions[1].title)
+
+  end
+  scenario "The titles of questions are hiperlinks" do
+    visit questions_path
+    expect(page).to have_link(questions[0].title)
+    expect(page).to have_link(questions[1].title)
   end
 end
