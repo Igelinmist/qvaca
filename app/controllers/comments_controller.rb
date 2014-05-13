8f832d9fae722201d767ef0a2a9997e0e15867d4
Vasiliy Troyan <trovas@inbox.ru>
2014-05-13 14:34:30 +0700

    Add comment for Question

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,16 +4,17 @@ class CommentsController < ApplicationController
   def create
     @commentable = @commentable_class.find(@commentable_id)
     @comment = @commentable.comments.create(comment_params)
+    @question = Question.find(params[:question_id])
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:commentable_id, :commentable_type, :body)
+    params.require(:comment).permit(:commentable_id, :commentable_type, :question_id, :body)
   end
 
   def setup_class
-    @commentable_class = params[:commentable_type].singularize.classify.constantize
     @commentable_id = params[:commentable_id]
+    @commentable_class = params[:commentable_type].classify.constantize
   end
 end
--- spec/acceptance/comment_question_spec.rb
+++ spec/acceptance/comment_question_spec.rb
@@ -6,19 +6,19 @@ feature 'User can comment question', %q{
   I want to comment the question
  } do
    given(:user) { create(:user) }
-   given(:question) { create(:question) }
+   given!(:question) { create(:question) }
    
-   scenario 'user can call the form for question comment and save comment' do
+   scenario 'user can call the form for question comment and save comment', js: true do
      sign_in(user)
      visit question_path question
      within '#question' do
        #click_on 'Добавить комментарий'
        fill_in 'comment_body', with: 'Мой комментарий'
-       click_on 'Сохранить'
+       click_on 'Комментировать'
      end
      
      expect(current_path).to eq question_path question
-     within '.question_comments' do
+     within '#question_comments' do
        expect(page).to have_content 'Мой комментарий'
      end
    end
