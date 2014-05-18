2c3820657b43a7e84ce8c3dda9767d1cb50a4505
Vasiliy Troyan <trovas@inbox.ru>
2014-05-18 14:10:08 +0700

    Answer edit correct

--- spec/acceptance/answer_edit_owner_spec.rb
+++ spec/acceptance/answer_edit_owner_spec.rb
@@ -24,8 +24,8 @@ feature 'Answer editing', %q{
     end
 
     scenario 'try to cancel edit answer', js: true do
-      click_on 'Редактировать'
       within "#answer-#{answer1.id}" do
+        click_on 'Редактировать'
         fill_in 'Ответ', with: 'Исправленный ответ'
         click_on 'Отмена'
         expect(page).to_not have_content 'Исправленный ответ'
