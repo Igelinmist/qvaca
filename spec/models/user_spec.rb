95b31f48255133d0b29463fec441cdd6919fe700
Vasiliy Troyan <trovas@inbox.ru>
2014-06-16 20:19:04 +0700

    Add facebook authorization

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -32,8 +32,7 @@ describe User do
         end
 
         it 'creates authorization with provider and uid' do
-          user = User.find_for_oauth(auth)
-          authorization = user.authorizations.first
+          authorization = User.find_for_oauth(auth).authorizations.first
 
           expect(authorization.provider).to eq auth.provider
           expect(authorization.uid).to eq auth.uid
@@ -42,6 +41,31 @@ describe User do
         it 'returns the user' do
           expect(User.find_for_oauth(auth)).to eq user
         end
+      end
+      context 'user does not exist' do
+        let(:auth) { OmniAuth::AuthHash.new(provider: 'facebook', uid: '123456', info: { email: 'new@user.com' }) }
+        it 'create new user' do
+          expect { User.find_for_oauth(auth) }.to change(User, :count).by(1)
+        end
+        it 'returns new user' do
+          expect(User.find_for_oauth(auth)).to be_a(User)
+        end
+        it 'fills user email' do
+          user = User.find_for_oauth(auth)
+
+          expect(user.email).to eq auth.info[:email]
+        end
+        it 'creates authorization for user' do
+          user = User.find_for_oauth(auth)
+          expect(user.authorizations).to_not be_empty
+        end
+        it 'creates authorization with provider and uid' do
+          authorization = User.find_for_oauth(auth).authorizations.first
+
+          expect(authorization.provider).to eq auth.provider
+          expect(authorization.uid).to eq auth.uid
+        end
+        
       end
     end
   end
