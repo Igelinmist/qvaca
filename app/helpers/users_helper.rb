f1a412c1aa68835e019e52cdf51599293ea9583b
Vasiliy Troyan <trovas@inbox.ru>
2014-06-18 09:24:25 +0700

    Some refactoring

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,14 @@
 module UsersHelper
   def setup_ext_attr(user)
     user.profile ||= user.build_profile
-    user.profile.display_name = session[:nickname] if session[:nickname]
-    user.profile.real_name = session[:name] if session[:name]
     user
+  end
+
+  def build_oauth_for(user)
+    if session[:provider]
+      user.authorizations.build(provider: session[:provider], uid: session[:uid])
+      user.profile.display_name = session[:nickname]
+      user.profile.real_name = session[:name]
+    end
   end
 end
