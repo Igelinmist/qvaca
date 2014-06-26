81e22082b3f4fdd65754b06d93530d5a80d443da
Vasiliy Troyan <trovas@inbox.ru>
2014-06-26 14:40:14 +0700

    Get all users_include_profile

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -1,6 +1,18 @@
 class Api::V1::ProfilesController < Api::V1::BaseController
   respond_to :json
+  
   def me
     respond_with current_resource_owner
   end
+
+  def index
+    respond_with profiles
+  end
+
+  protected
+
+  def profiles
+    @profiles ||= User.all.to_json(include: :profile)
+  end
+
 end
--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -42,29 +42,46 @@ describe "Profiles API" do
   describe "All profiles" do
     context 'unathorized' do
       it 'returns 401 status code if there is no access_token' do
-        get '/api/v1/profiles/list', format: :json
+        get '/api/v1/profiles', format: :json
         expect(response.status).to eq 401
       end
 
       it 'returns 401 status code if access_token is invalid' do
-        get '/api/v1/profiles/list', format: :json, access_token: '1234'
+        get '/api/v1/profiles', format: :json, access_token: '1234'
         expect(response.status).to eq 401
       end
     end
 
     context 'authorized' do
-      let(:me) { create(:user) }
-      let(:users) { create_pair(:user) }
+      let!(:me) { create(:user) }
+      let!(:user) { create(:user, display_name: 'BadBoy') }
       let(:access_token) { create(:access_token, resource_owner_id: me.id) }
       before do
-        get '/api/v1/profiles/list', format: :json, access_token: access_token.token
+        get '/api/v1/profiles', format: :json, access_token: access_token.token
       end
 
       it 'returns 200 status code' do
         expect(response).to be_success
       end
 
-      
+      %w(id email created_at).each do |attr|
+        it "returns some user #{attr}" do
+          expect(response.body).to be_json_eql(user.send(attr.to_sym).to_json).at_path("1/#{attr}")
+        end
+      end
+
+      %w(display_name real_name).each do |attr|
+        it "returns some user's profile data #{attr}" do
+          expect(response.body).to be_json_eql(user.profile.send(attr.to_sym).to_json).at_path("1/profile/#{attr}")
+        end
+      end
+
+      %w(password encrypted_password).each do |attr|
+        it "does not contain some user #{attr}" do
+          expect(response.body).to_not have_json_path("1/#{attr}")
+        end
+      end
+
     end
   end
 end
