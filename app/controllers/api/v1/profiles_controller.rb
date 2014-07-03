6cc67055e72246ee5fcf40e29f341d694109d445
Vasiliy Troyan <trovas@inbox.ru>
2014-07-03 14:48:37 +0700

    Little clearing for api-profile-controller

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -1,18 +1,20 @@
 class Api::V1::ProfilesController < Api::V1::BaseController
+  defaults :resource_class => User, :collection_name => 'users', :instance_name => 'user'
   respond_to :json
+  actions :index
   
   def me
     respond_with current_resource_owner
   end
 
-def index
-    respond_with profiles.to_json(include: :profile)
+  def index
+    respond_with collection.to_json(include: :profile)
   end
 
   protected
 
-  def profiles
-    @profiles ||= User.all.includes :profile
+  def collection
+    collection ||= User.all.includes :profile
   end
 
 end
