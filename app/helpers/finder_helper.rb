a44d1a74e5c798ac5ef574fcf04105840100434e
Vasiliy Troyan <trovas@inbox.ru>
2014-08-28 16:25:12 +0700

    Search result as link

--- app/helpers/finder_helper.rb
+++ app/helpers/finder_helper.rb
@@ -1,2 +1,9 @@
 module FinderHelper
+  def search_link(content)
+    if content.class.to_s == 'Question'
+      link_to truncate(content.title, length: 80), content
+    else
+      link_to truncate(content.body, length: 80), content.question
+    end
+  end
 end
